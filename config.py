8b12615bf2b7d91cfc59487231a7e1241a548dea
penglee87 <penglee87@163.com>
2016-07-01 15:35:00 +0800

    first create

---
+++ config.py
@@ -0,0 +1,52 @@
+import os
+import pymysql
+pymysql.install_as_MySQLdb()
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    MAIL_SERVER = 'smtp.163.com'
+    MAIL_PORT = 25
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'jqblee@163.com'
+    MAIL_PASSWORD = 'jq000111'
+    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
+    FLASKY_MAIL_SENDER = 'jqblee@163.com'
+    FLASKY_ADMIN = 'penglee87@163.com'
+    FLASKY_POSTS_PER_PAGE = 20
+    FLASKY_FOLLOWERS_PER_PAGE = 50
+    FLASKY_COMMENTS_PER_PAGE = 30
+    DEBUG = True
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or 'mysql://flasky:flasky@127.0.0.1/flasky'
+        #'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+        #'mysql+pymysql://flask:flask@127.0.0.1/flask'
+        #'mysql://flasky:flasky@127.0.0.1/flasky'
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+
+    'default': DevelopmentConfig
+}
