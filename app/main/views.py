e620b6c695fc0635e69ade9bc3e6a0fdafadf50f
penglee87 <penglee87@163.com>
2016-08-12 17:31:34 +0800

    201608121800

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,5 @@
-from flask import render_template, redirect, url_for, abort, flash, request,\
-    current_app, make_response
-from flask.ext.login import login_required, current_user
+from flask import render_template, redirect, url_for, abort, flash, request,current_app, make_response
+from flask_login import login_required, current_user
 from . import main
 from .forms import EditProfileForm, EditProfileAdminForm, PostForm,CommentForm,EditForm
 from .. import db
